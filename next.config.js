e2ba9cf05c03099f5dccbea396bbcd9e9d262d98
siyanda sokhela <SiyandaSokhela@gmail.com>
2024-12-04 10:50:06 +0200

    Backend - implementation
    Get user information

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/**
+ * @format
+ * @type {import('next').NextConfig}
+ */
 
-module.exports = nextConfig
+const nextConfig = {
+  images: {
+    domains: [
+      "plus.unsplash.com",
+      "i2.wp.com",
+      "www.cameo.com",
+      "mir-s3-cdn-cf.behance.net",
+    ],
+  },
+};
+
+module.exports = nextConfig;
